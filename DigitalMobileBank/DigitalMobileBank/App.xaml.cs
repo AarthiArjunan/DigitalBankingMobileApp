246290ffeb5d7a945f8b9bd153e8d22615998d84
AARTHI <aarthi.sf3966@gmail.com>
2024-12-24 19:14:03 +0530

    Added project

---
+++ DigitalMobileBank/DigitalMobileBank/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DigitalMobileBank
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DigitalMobileBank/DigitalMobileBank/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalMobileBank
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
